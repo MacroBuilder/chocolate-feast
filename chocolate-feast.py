8636fe2d15a635e6433721e44e54a2ed9758fc30
T.C. <thomas.n.cool@gmail.com>
2015-06-08 13:34:59 -0700

    Create chocolate-feast.py

---
+++ chocolate-feast.py
@@ -0,0 +1,17 @@
+def chocolate(n,c,m):
+    bars = n/c
+
+    wrappers = bars
+
+    while wrappers >=m:
+        trade = wrappers/m
+        bars += trade
+        wrappers %= m
+        wrappers += trade
+        
+    print bars
+
+test_cases = int(raw_input().strip())
+for _ in range(test_cases):
+    A,B,C1 = [int(x) for x in raw_input().split()]
+    Choco(A,B,C1)
